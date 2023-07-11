505450e4fc73c4712485ea770a2d696d0f62a22d
Jorge <jgarciatorralba@gmail.com>
2023-07-11 21:22:52 +0200

    add "use" statement

--- src/Shared/Infrastructure/Bus/Exception/CommandNotRegisteredException.php
+++ src/Shared/Infrastructure/Bus/Exception/CommandNotRegisteredException.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace App\Shared\Infrastructure\Bus\Exception;
 
 use App\Shared\Domain\Bus\Command\Command;
+use Exception;
 
-class CommandNotRegisteredException extends \Exception
+class CommandNotRegisteredException extends Exception
 {
     public function __construct(Command $command)
     {
