18f0d45fc44e6ecaa7450ef6ab30e273eb847509
Jorge <jgarciatorralba@gmail.com>
2023-07-15 21:43:30 +0200

    create domain aggregates Root and Timestampable

---
+++ src/Shared/Domain/Aggregate/AggregateRoot.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain\Aggregate;
+
+use App\Shared\Domain\Bus\Event\DomainEvent;
+
+abstract class AggregateRoot
+{
+    private array $events = [];
+
+    final public function pullEvents(): array
+    {
+        $events = $this->events;
+        $this->events = [];
+
+        return $events;
+    }
+
+    final protected function recordEvent(DomainEvent $event): void
+    {
+        $this->events[] = $event;
+    }
+
+    abstract public function toArray(): array;
+}
---
+++ src/Shared/Domain/Aggregate/Timestampable.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Domain\Aggregate;
+
+use DateTimeImmutable;
+
+trait Timestampable
+{
+    private DateTimeImmutable $createdAt;
+    private DateTimeImmutable $updatedAt;
+    private DateTimeImmutable $deletedAt;
+
+    public function updatedAt(): DateTimeImmutable
+	{
+		return $this->updatedAt;
+	}
+
+	public function createdAt(): DateTimeImmutable
+	{
+		return $this->createdAt;
+	}
+
+    public function deletedAt(): DateTimeImmutable
+	{
+		return $this->deletedAt;
+	}
+
+    public function updateCreatedAt(DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function updateUpdatedAt(DateTimeImmutable $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function updateDeletedAt(?DateTimeImmutable $deletedAt): void
+    {
+        $this->deletedAt = $deletedAt;
+    }
+}
