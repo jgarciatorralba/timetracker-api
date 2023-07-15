83a306a579690ad34e03d4ae4c1981da07544d9c
Jorge <jgarciatorralba@gmail.com>
2023-07-15 22:50:34 +0200

    create Uuid Doctrine DBAL type

---
+++ src/Shared/Infrastructure/Persistence/Doctrine/Type/UuidType.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Infrastructure\Persistence\Doctrine\Type;
+
+use App\Shared\Domain\ValueObject\Uuid;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\ConversionException;
+use InvalidArgumentException;
+use Symfony\Component\Uid\Uuid as SymfonyUuid;
+
+final class UuidType extends Type
+{
+    const NAME = 'uuid';
+
+    public function getName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform): string
+    {
+        return $platform->getBinaryTypeDeclarationSQL([
+            'length' => '16',
+            'fixed' => true,
+        ]);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?Uuid
+    {
+        if ($value instanceof Uuid || $value === null) {
+            return $value;
+        }
+
+        if (!is_string($value)) {
+            throw ConversionException::conversionFailedInvalidType($value, $this->getName(), ['null', 'string', Uuid::class]);
+        }
+
+        try {
+            return new Uuid(SymfonyUuid::fromBinary($value)->toRfc4122());
+        } catch (InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+        }
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        $toBinary = fn(string $value): string => SymfonyUuid::fromString($value)->toBinary();
+
+        if ($value instanceof Uuid) {
+            return $toBinary($value->value());
+        }
+
+        if (null === $value || $value === '') {
+            return null;
+        }
+
+        if (!is_string($value)) {
+            throw ConversionException::conversionFailedInvalidType($value, $this->getName(), ['null', 'string', AbstractUid::class]);
+        }
+
+        try {
+            return $toBinary($value);
+        } catch (InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, $this->getName(), $e);
+        }
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return false;
+    }
+}
