aa338f33b5a5a2364acfa20f458d64c2043dac24
Jorge <jgarciatorralba@gmail.com>
2023-07-16 19:49:52 +0200

    add validator class

---
+++ src/UI/Validation/Validator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\UI\Validation;
+
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class Validator
+{
+    private ValidatorInterface $validator;
+
+    public function __construct()
+    {
+        $this->validator = Validation::createValidator();    
+    }
+
+    public function validate(array $data, Assert\Collection $rules): array
+    {
+        $errors = [];
+        $violations = $this->validator->validate($data, $rules);
+        if ($violations->count()) {
+            /** @var ConstraintViolationInterface $violation */
+            foreach($violations as $violation) {
+                $fieldName = $this->parsePropertyPath($violation->getPropertyPath());
+                $errors[$fieldName] = $violation->getMessage();
+            }
+        }
+
+        return $errors;
+    }
+
+    private function parsePropertyPath(string $propertyPath): string 
+    {
+        return substr($propertyPath, 1, -1);
+    }
+}
