c04bdc3907a5fbf81af7b001ee4c889bb00740c0
Jorge <jgarciatorralba@gmail.com>
2023-07-16 10:03:20 +0200

    create new migration

--- migrations/Version20230716080226.php
+++ migrations/Version20230716080226.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230715213030 extends AbstractMigration
+final class Version20230716080226 extends AbstractMigration
 {
     public function getDescription(): string
     {
