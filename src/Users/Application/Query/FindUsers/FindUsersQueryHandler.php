26578508b0a28da5cb0f2e7ee8a066e2f1856efc
Jorge <jgarciatorralba@gmail.com>
2023-07-16 19:55:56 +0200

    update FindUsersQueryHandler

--- src/Users/Application/Query/FindUsers/FindUsersQueryHandler.php
+++ src/Users/Application/Query/FindUsers/FindUsersQueryHandler.php
@@ -4,24 +4,20 @@ declare(strict_types=1);
 
 namespace App\Users\Application\Query\FindUsers;
 
+use App\Shared\Domain\Aggregate\AggregateRoot;
 use App\Shared\Domain\Bus\Query\QueryHandler;
+use App\Users\Domain\Contract\UserRepository;
 
 final class FindUsersQueryHandler implements QueryHandler
 {
-    public function __construct() {}
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
 
     public function __invoke(FindUsersQuery $query): FindUsersResponse
     {
-        $users = [
-            [
-                'id' => 1,
-                'name' => 'First user name'
-            ],
-            [
-                'id' => 2,
-                'name' => 'Second user name'
-            ]
-        ];
+        $users = $this->userRepository->findAll();
+        $users = array_map(fn (AggregateRoot $item) => $item->toArray(), $users);
         
         return new FindUsersResponse($users);
     }
