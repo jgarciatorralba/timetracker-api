d9c12ab0c741d356b356a117a3fb141738e1558b
Jorge García <jgarciatorralba@gmail.com>
2023-07-22 11:19:43 +0200

    handle query param in GetWorkEntriesController

--- src/UI/Controller/WorkEntries/GetWorkEntriesController.php
+++ src/UI/Controller/WorkEntries/GetWorkEntriesController.php
@@ -5,16 +5,20 @@ declare(strict_types=1);
 namespace App\UI\Controller\WorkEntries;
 
 use App\UI\Controller\BaseController;
+use App\UI\Request\WorkEntries\GetWorkEntriesRequest;
 use App\WorkEntries\Application\Query\FindWorkEntries\FindWorkEntriesQuery;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 final class GetWorkEntriesController extends BaseController
 {
-    public function __invoke(): Response
+    public function __invoke(GetWorkEntriesRequest $request): Response
     {
-        $response = $this->ask(new FindWorkEntriesQuery());
+        $userIds = getType($request->get('userId')) === 'string'
+            ? [$request->get('userId')]
+            : $request->get('userId');
 
+        $response = $this->ask(new FindWorkEntriesQuery($userIds ?? []));
         return new JsonResponse($response->data(), Response::HTTP_OK);
     }
 }
