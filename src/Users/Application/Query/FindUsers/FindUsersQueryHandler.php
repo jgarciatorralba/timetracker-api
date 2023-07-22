9a2826f0924ed3038d4cdcd93f18feb0a0a8738c
Jorge García <jgarciatorralba@gmail.com>
2023-07-22 11:18:14 +0200

    update service naming in FindUsersQueryHandler

--- src/Users/Application/Query/FindUsers/FindUsersQueryHandler.php
+++ src/Users/Application/Query/FindUsers/FindUsersQueryHandler.php
@@ -6,18 +6,18 @@ namespace App\Users\Application\Query\FindUsers;
 
 use App\Shared\Domain\Aggregate\AggregateRoot;
 use App\Shared\Domain\Bus\Query\QueryHandler;
-use App\Users\Domain\Service\FindUsers;
+use App\Users\Domain\Service\FindUsersByCriteria;
 
 final class FindUsersQueryHandler implements QueryHandler
 {
     public function __construct(
-        private readonly FindUsers $findUsers
+        private readonly FindUsersByCriteria $findUsersByCriteria
     ) {
     }
 
     public function __invoke(FindUsersQuery $query): FindUsersResponse
     {
-        $users = $this->findUsers->__invoke();
+        $users = $this->findUsersByCriteria->__invoke();
         $users = array_map(fn (AggregateRoot $user) => $user->toArray(), $users);
 
         return new FindUsersResponse($users);
