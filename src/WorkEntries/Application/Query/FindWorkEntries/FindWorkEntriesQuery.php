8a9425e1963e6f3b6757fcf7cfc891f2e6c9fcdb
Jorge García <jgarciatorralba@gmail.com>
2023-07-22 11:22:16 +0200

    update query and queryHandler to consider query param

--- src/WorkEntries/Application/Query/FindWorkEntries/FindWorkEntriesQuery.php
+++ src/WorkEntries/Application/Query/FindWorkEntries/FindWorkEntriesQuery.php
@@ -8,4 +8,16 @@ use App\Shared\Domain\Bus\Query\Query;
 
 final class FindWorkEntriesQuery implements Query
 {
+    public function __construct(
+        private readonly array $userIds
+    ) {
+    }
+
+    /**
+     * @return array<string>
+     */
+    public function userIds(): array
+    {
+        return $this->userIds;
+    }
 }
--- src/WorkEntries/Application/Query/FindWorkEntries/FindWorkEntriesQueryHandler.php
+++ src/WorkEntries/Application/Query/FindWorkEntries/FindWorkEntriesQueryHandler.php
@@ -6,17 +6,37 @@ namespace App\WorkEntries\Application\Query\FindWorkEntries;
 
 use App\Shared\Domain\Aggregate\AggregateRoot;
 use App\Shared\Domain\Bus\Query\QueryHandler;
+use App\Shared\Domain\ValueObject\Uuid;
+use App\Users\Domain\Service\FindUsersByCriteria;
 use App\WorkEntries\Domain\Service\FindWorkEntries;
 
 final class FindWorkEntriesQueryHandler implements QueryHandler
 {
     public function __construct(
+        private readonly FindUsersByCriteria $findUsersByCriteria,
         private readonly FindWorkEntries $findWorkEntries
     ) {
     }
 
     public function __invoke(FindWorkEntriesQuery $query): FindWorkEntriesResponse
     {
+        if (!empty($userIds = $query->userIds())) {
+            $uuids = array_map(fn (string $userId) => Uuid::fromString($userId), $userIds);
+            $users = $this->findUsersByCriteria->__invoke(['id' => $uuids]);
+
+            $workEntries = [];
+            foreach ($users as $user) {
+                $workEntries = array_merge($workEntries, $user->workEntries()->toArray());
+            }
+            
+            return new FindWorkEntriesResponse(
+                array_map(
+                    fn (AggregateRoot $workEntry) => $workEntry->toArray(),
+                    $workEntries
+                )
+            );
+        }
+
         $workEntries = $this->findWorkEntries->__invoke();
         $workEntries = array_map(fn (AggregateRoot $workEntry) => $workEntry->toArray(), $workEntries);
 
